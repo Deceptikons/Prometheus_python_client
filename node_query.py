d1394e03a41ac9fbe21f0b37b59cdbad459abbe6
YasaswiK <yasaswik.bcl2@gmail.com>
2017-02-19 16:18:36 +0530

    Added utilization stats

--- node_query.py
+++ node_query.py
@@ -11,19 +11,25 @@ from pyspark.mllib.regression import LabeledPoint'''
 # we periodically send this request every 10(?) seconds 
 overall_stats = {}
 # Name of all the stats we want to query prometheus for
-stats_name_list = ["node_cpu"] 
+stats_name_list = ["(avg by (instance) (irate(node_cpu{mode='idle'}[5m])) * 100)"] 
 def collect_stats():
-	global overall_stats
-	for i in overall_stats.keys():
-		dictionary = query.sendRequest(i)
-		print dictionary
+  global overall_stats
+  for i in overall_stats.keys():
+    dictionary = query.sendRequest(i)
+    for key in dictionary.keys():
+      if key in overall_stats[i]:
+        overall_stats[i][key].append(dictionary[key])
+      else:
+        overall_stats[i][key] = [dictionary[key]]
+    print dictionary
+    print overall_stats
 
 def init():
-	global stats_name_list
-	global overall_stats
-	for name in stats_name_list:
-		overall_stats[name] = {}
+  global stats_name_list
+  global overall_stats
+  for name in stats_name_list:
+    overall_stats[name] = {}
 
 init()
 for i in tqdm.tqdm(range(1000)):
-	collect_stats()
+  collect_stats()
--- query.py
+++ query.py
@@ -1,8 +1,8 @@
 ''' TODO: 
-	URL is hardcoded as localhost here - make it generic
-	Separate functions for separate request types to handle data differently
-	Error handling 
-'''		
+  URL is hardcoded as localhost here - make it generic
+  Separate functions for separate request types to handle data differently
+  Error handling 
+'''   
 import requests
 import json
 import pprint
@@ -13,44 +13,48 @@ import datetime
 # for metadata and [data][result][value][1] fo hte data
 # if the query is for a stat from node_exporter the format is
 # [data][result][metric] gives an object for the metadata
-def request(request_type, query, start=None, end=None, step=None, logging_level=0, source_type="node"):
-	if (start!=None and end!=None and step!=None):
-		string = 'http://localhost:9090/api/v1/' + request_type + '?query=' + query + '&start='+start+'&end='+end + '&step=' + step
-	else:
-		string = 'http://localhost:9090/api/v1/' + request_type + '?query=' + query 
-	response = requests.get(string)
-	decoded_obj = json.loads(response.content)
-	if (logging_level==2):
-		pp = pprint.PrettyPrinter(depth=6)
-		pp.pprint(decoded_obj)
-	result  = decoded_obj['data']['result']
-	if (logging_level==2):
-		# print the JSON nicely
-		pp.pprint(result)
-	returned_values = {}# the dictionary of values we return
-	for iterable in result:
-		if (source_type=="jmx"):
-			# if the query was a range type
-			if (request_type=='query_range'):
-				values = iterable['values']
-				for i in values:
-					returned_values[iterable['metric']['scope']]=float(i[1])
-			elif (request_type=='query'):
-				returned_values[iterable['metric']['scope']]=float(iterable['value'][1])
-		elif (source_type=="node"):
-			#assuming query is not a range type here
-			# build the key
-			key=iterable['metric']['instance']+iterable['metric']['cpu']
-			returned_values[iterable['metric']]=float(iterable['value'][1])
-	if (logging_level==1):
-		print returned_values
-	return returned_values
+def request(request_type, query, start=None, end=None, step=None, logging_level=0, source_type="node_hybrid"):
+  if (start!=None and end!=None and step!=None):
+    string = 'http://localhost:9090/api/v1/' + request_type + '?query=' + query + '&start='+start+'&end='+end + '&step=' + step
+  else:
+    string = 'http://localhost:9090/api/v1/' + request_type + '?query=' + query 
+  response = requests.get(string)
+  decoded_obj = json.loads(response.content)
+  if (logging_level==2):
+    pp = pprint.PrettyPrinter(depth=6)
+    pp.pprint(decoded_obj)
+  result  = decoded_obj['data']['result']
+  if (logging_level==2):
+    # print the JSON nicely
+    pp.pprint(result)
+  returned_values = {}# the dictionary of values we return
+  for iterable in result:
+    if (source_type=="jmx"):
+      # if the query was a range type
+      if (request_type=='query_range'):
+        values = iterable['values']
+        for i in values:
+          returned_values[iterable['metric']['scope']]=float(i[1])
+      elif (request_type=='query'):
+        returned_values[iterable['metric']['scope']]=float(iterable['value'][1])
+    elif (source_type=="node_atomic"):
+      # assuming query is not a range type here
+      # build the key
+      key=iterable['metric']['instance']+iterable['metric']['cpu']+ iterable['metric']['mode']
+      returned_values[key]=float(iterable['value'][1])
+    elif (source_type=="node_hybrid"):
+      key=iterable['metric']['instance']
+      print key
+      returned_values[key]=float(iterable['value'][1])
+    if (logging_level==1):
+      print returned_values
+    return returned_values
 
 
 # testing with a particular query
 
 def sendRequest(query):
-	start = str(time.time())
-	end = str(time.time()+300)
-	return request('query', query, start, end,logging_level=2)
+  start = str(time.time())
+  end = str(time.time()+300)
+  return request('query', query, start, end,logging_level=1)
 
