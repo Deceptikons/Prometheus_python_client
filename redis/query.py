8b99e1e658dc18b62cd2930d0385ca456aea6cf6
YasaswiK <yasaswik.bcl2@gmail.com>
2017-04-02 16:07:08 +0530

    Added a Redis-backed store for application metrics

---
+++ redis/query.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import redis
+import time
+
+# In the web service, this should be created only once,
+#	not for every request
+r = redis.StrictRedis()
+
+# The query should be a dictionary/JSON passed 
+# it should contain two key-value pairs - query and the instance
+def queryStats(querydict):
+	# we first get all the keys
+	key_list = r.smembers("keyset")
+	#print key_list
+	keys_to_query = set()
+	for key in key_list:
+		if (querydict["attribute"] in key):
+			if ("instance" in querydict.keys() and querydict["instance"] in key):
+				keys_to_query.add(key)
+			else:
+				keys_to_query.add(key)
+	print keys_to_query
+	# we query for all these keys now. We split the key into the following
+	# query - the part before the first ','
+	# split all the other key-value pairs using '='
+	return_dict= {}
+	for key in keys_to_query:
+		temp_dict = {}
+		pairs = key.split(",")
+		temp_dict["query"] = pairs[0] 
+		for pair in pairs[1:]:
+			innerpair = pair.split("=")
+			#print	innerpair 
+			if (len(innerpair)==2):
+				temp_dict[innerpair[0]] = innerpair[1] 
+		# Finally we query. We return values from the last hour
+		timestamp = time.time()
+		values = r.zrangebyscore(key,timestamp-3600, timestamp)
+		temp_dict["values"] = values
+		print temp_dict
+
+
+if (__name__=="__main__"):
+	# test for this
+	print "hello"
+	querydict = {}
+	querydict["attribute"] = "org_apache_cassandra_metrics_clientrequest_98thpercentile"
+	queryStats(querydict)
+
+	querydict = {}
+	querydict["attribute"] = "node_cpu"
+	queryStats(querydict)
+
+			
+
---
+++ redis/scraper.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import requests 
+import time
+import redis
+import tqdm
+from mesosdnsclient import request
+node_exporter_uri = "http://localhost:7070/metrics"
+def scrape(uri):
+	page = requests.get(uri)
+	timestamp = time.time()
+	#print page.content
+	data_lines = page.content.split("\n")
+	r = redis.StrictRedis()
+	for line in data_lines:
+		if (line):
+			# we ignore lines starting with #
+			if (line[0]!='#'):
+				#print line
+				# format is main-attribute{key1="value1", key2... } actual value
+				# we first get the entire key
+				if "{" in line: 
+					key,key1 = line.split("{")
+					key2, value = key1.split("}")
+					final_key = key +","  + key2
+					r.zadd(final_key, timestamp, value)
+					#r.lpush("keys", final_key)
+					r.sadd("keyset", final_key)
+				# Single key-value pair - can be separated by space
+				else: 
+					final_key,value = line.split(" ")
+					r.zadd(final_key, timestamp, value)
+					r.sadd("keyset", final_key)
+					#r.lpush("keys", final_key)
+
+# we print all keys
+#print r.lrange("keys", 0, -1)
+
+# this function needs to be called before scrape everytime
+# returns a list of uris to hit
+def init_uris(): 
+	# we call mesos-dns - we hardcode IP to localhost for now
+	# services are cassandra-seed and test-app for now 
+	# All ports are 7070 for JMX and 9100 for node
+	seed_ip = request("localhost", "cassandraseed")
+	uri_list = []
+	if (len(seed_ip)!=0):
+		seed_uri = "http://" + seed_ip[0] + ":7070/metrics"
+		uri_list.append(seed_uri)
+		# we get a list of test-app instances
+		non_seed_ips = request("localhost", "test-app")
+		for ip in non_seed_ips:
+			if (ip!=""):
+				non_seed_uri = "http://" + ip + ":7070/metrics"
+				uri_list.append(non_seed_uri)
+
+	# we now add the node_exporter uris
+	node_exporter_uri = "http://localhost:9100/metrics"
+	uri_list.append(node_exporter_uri)
+	return uri_list
+	
+
+
+if (__name__=="__main__"):
+	node_exporter_uri = "http://localhost:9100/metrics"
+	for i in tqdm.tqdm(range(500)):
+		uri_to_scrape = init_uris()
+		print uri_to_scrape
+		for uri in uri_to_scrape:
+			scrape(uri)
+		time.sleep(1)
+	for key in r.lrange("keys", 0, -1):
+		timestamp = time.time()
+		print key ,": ", r.zrangebyscore(key, 0, timestamp+100001)
+
+
